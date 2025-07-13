89437e2f28e0f5a12264a9ce7da87568aa31e907
Jayaprakash_B <138005233+Jp2506@users.noreply.github.com>
2025-07-13 13:28:11 +0530

    Time: 30 ms (48.66%), Space: 80.4 MB (5.47%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -6,18 +6,22 @@ public:
         sort(players.begin(),players.end());
         sort(trainers.begin(),trainers.end());
 
-        for(int i=0;i<players.size();i++)
+        int i=0,j=0;
+        while(i<players.size())
         {
-            for(int j=0;j<trainers.size();j++)
+            while(j<trainers.size())
             {
                 if(players[i]<=trainers[j])
                 {
-                    count++;
-                    auto it=find(trainers.begin(),trainers.end(),trainers[j]);
-                    trainers.erase(it);
-                    break;
+                count++;
+                j++;
+                break;
                 }
+                
+                j++;
             }
+            i++;
+            
         }
         return count;
     }
